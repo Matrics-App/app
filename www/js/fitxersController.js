fe06363524f4e72e91183097c62eb773bb5de263
Xavi <57891264+Xavi-PL@users.noreply.github.com>
2021-05-05 15:51:50 +0200

    FIXED: Añadio semaforo totalmente apagado

--- www/js/fitxersController.js
+++ www/js/fitxersController.js
@@ -6,7 +6,8 @@ function getRequisits(){
 
     var arrayStatus = ['<td class="center"><i class="material-icons circle red-text custom-icon-padding">brightness_1</i><i style="opacity:0.3" class="material-icons circle amber-text custom-icon-padding">brightness_1</i><i style="opacity:0.3" class="material-icons circle green-text custom-icon-padding">brightness_1</i></td>',
         '<td class="center"><i style="opacity:0.3" class="material-icons circle red-text custom-icon-padding">brightness_1</i><i class="material-icons circle amber-text custom-icon-padding">brightness_1</i><i style="opacity:0.3" class="material-icons circle green-text custom-icon-padding">brightness_1</i></td>',
-        '<td class="center"><i style="opacity:0.3" class="material-icons circle red-text custom-icon-padding">brightness_1</i><i style="opacity:0.3" class="material-icons circle amber-text custom-icon-padding">brightness_1</i><i class="material-icons circle green-text custom-icon-padding">brightness_1</i></td>']
+        '<td class="center"><i style="opacity:0.3" class="material-icons circle red-text custom-icon-padding">brightness_1</i><i style="opacity:0.3" class="material-icons circle amber-text custom-icon-padding">brightness_1</i><i class="material-icons circle green-text custom-icon-padding">brightness_1</i></td>',
+        '<td class="center"><i style="opacity:0.3" class="material-icons circle red-text custom-icon-padding">brightness_1</i><i style="opacity:0.3" class="material-icons circle amber-text custom-icon-padding">brightness_1</i><i style="opacity:0.3" class="material-icons circle green-text custom-icon-padding">brightness_1</i></td>']
 
     var paneRequisits = $('#paneRequisits');
     var tempArray = '["DNI","TOMATE","PATATA"]';
@@ -21,9 +22,7 @@ function getRequisits(){
             "html": '<div style="display:flex; flex-direction:row;"><h6 id="nameRequisit' + i + '">' + arrayRequisits[i] + '</h6><a id="btnRequisit' + i + '" class="custom-margin-left-1em custom-border-radius blue-gradient waves-effect waves-light btn">AFEGEIX!</a>' + arrayStatus[i]
         }).appendTo(paneRequisits);
         $('btnRequisit' + i).on('click', function(){
-
             console.log($('nameRequisit0').val());
-
         })
     }
 }
